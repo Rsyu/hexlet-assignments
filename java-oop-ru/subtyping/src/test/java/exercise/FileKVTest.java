c70e92f10b66ac0b2e88c48f91e4bf2c8cc14e35
@hexlet/cli <support@hexlet.io>
2025-04-04 11:06:14 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
+++ java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
@@ -1,11 +1,16 @@
 package exercise;
 
-import java.util.HashMap;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 // BEGIN
 
@@ -24,38 +29,31 @@ class FileKVTest {
     }
 
     // BEGIN
- @Test
+   @Test
     void testSetGetUnset() {
         KeyValueStorage storage = new FileKV(filepath.toString(), Map.of("key", "value"));
 
-        // get existing key
         assertThat(storage.get("key", "default")).isEqualTo("value");
-
-        // get missing key
         assertThat(storage.get("unknown", "default")).isEqualTo("default");
 
-        // set new key
         storage.set("name", "Hexlet");
         assertThat(storage.get("name", "default")).isEqualTo("Hexlet");
 
-        // unset key
         storage.unset("name");
         assertThat(storage.get("name", "default")).isEqualTo("default");
 
-        // toMap
         assertThat(storage.toMap()).isEqualTo(Map.of("key", "value"));
     }
 
     @Test
-    void testSwapKeyValue() {
+    void testSwap() {
         KeyValueStorage storage = new FileKV(filepath.toString(), Map.of("a", "1", "b", "2"));
+
         App.swapKeyValue(storage);
 
-        // Проверяем, что значения и ключи поменялись
         assertThat(storage.get("1", "default")).isEqualTo("a");
         assertThat(storage.get("2", "default")).isEqualTo("b");
         assertThat(storage.get("a", "default")).isEqualTo("default");
-    }
-    
+    }    
     // END
 }

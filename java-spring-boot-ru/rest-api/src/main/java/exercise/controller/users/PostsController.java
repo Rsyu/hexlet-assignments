121ab5eff404f63d541111e86cbfdd53715d3d18
@hexlet/cli <support@hexlet.io>
2025-05-13 17:41:39 +0300

    submit java-spring-boot-ru/rest-api

---
+++ java-spring-boot-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -0,0 +1,42 @@
+package exercise.controller.users;
+
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import exercise.model.Post;
+import exercise.Data;
+
+// BEGIN
+@RestController
+@RequestMapping("/api/users/{id}/posts")
+public class PostsController {
+
+    private List<Post> posts = Data.getPosts();
+
+    // Получение всех постов пользователя
+    @GetMapping
+    public ResponseEntity<List<Post>> getUserPosts(@PathVariable String id) {
+        List<Post> userPosts = posts.stream()
+            .filter(post -> post.getUserId().equals(id))
+            .collect(Collectors.toList());
+
+        return ResponseEntity.ok(userPosts);
+    }
+
+    // Создание нового поста пользователя
+    @PostMapping
+    public ResponseEntity<Post> createUserPost(@PathVariable String id, @RequestBody Post post) {
+        post.setUserId(id);
+        post.setId("post" + (posts.size() + 1));
+        posts.add(post);
+        return ResponseEntity.status(HttpStatus.CREATED).body(post);
+    }
+}
+// END
